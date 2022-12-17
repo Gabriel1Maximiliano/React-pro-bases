a5c832922bc379f4ca84485a6e7f463dca980b40
Gabriel1Maximiliano <gabrielhmloza@gmail.com>
2022-12-17 20:08:40 -0300

    it splits actions reducer and interfaces in folders

---
+++ src/counter-reducer/CounterReducer.tsx
@@ -0,0 +1,38 @@
+import React, { useReducer } from 'react';
+import { CounterState } from './interfaces/interfaces';
+import { counterComponentReducer } from './state/counterReducer';
+
+
+const INITIAL_STATE:CounterState ={
+    counter:0,
+    previous:0,
+    changes:0
+}
+
+
+export const CounterReducer = () => {
+
+    const [state, dispatch] = useReducer(counterComponentReducer, INITIAL_STATE)
+
+    const handleReset = () => {
+        dispatch({type:'reset'});
+    }
+    const increaseBy = (value:number) => {
+        dispatch({ type:'increaseBy', payload:{value}});
+
+    }
+  return (
+   <>
+   <h1>Counter Reducer: { state.counter }</h1>
+   <pre>
+    {
+        JSON.stringify( state,null,3)
+    }
+   </pre>
+   <button onClick={ handleReset } >Reset</button>
+   <button onClick={ ()=>increaseBy(1) } >+1</button>
+   <button onClick={ ()=>increaseBy(5) } >+5</button>
+   <button onClick={ ()=>increaseBy(10) } >+10</button>
+   </>
+  )
+}
