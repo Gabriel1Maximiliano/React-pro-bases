20d404232fdd2e97971eaf2f666eddd31d726d03
Gabriel1Maximiliano <gabrielhmloza@gmail.com>
2022-12-17 18:43:36 -0300

    useRef and timelines in gsp implemented

--- src/bases/CounterEffect.tsx
+++ src/bases/CounterEffect.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { gsap } from "gsap";
 const MAXIMUN_COUNT = 10;   
 export const CounterEffect = () => {
@@ -12,20 +12,24 @@ export const CounterEffect = () => {
         setCounter(prev =>  prev+1);
     }
 
+    const counterElement = useRef<HTMLHeadingElement>(null)
+
     useEffect(() => {
       if( counter < 10 ){
         return 
       }
-    
-      gsap.to('h2',{y:-10,duration:0.2,ease:'ease.out'}).then(() => {
-        gsap.to('h2',{y:0,duration:0.2,ease:'bounce.out'})
-      });
+
+      const tl = gsap.timeline();
+      tl.to(counterElement.current,{y:-10,duration:0.2,ease:'ease.out'});
+      tl.to(counterElement.current,{y:0,duration:1,ease:'bounce.out'});
+      
+     
     }, [counter])
     
   return (
    <>
    <h1>CounterEffect : </h1>
-   <h2>{ counter }</h2>
+   <h2 ref={ counterElement } >{ counter }</h2>
    <button onClick={ handleClick } >+1</button>
    </>
   )
